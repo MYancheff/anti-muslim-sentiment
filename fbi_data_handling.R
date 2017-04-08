51c2426b69e1ac7f8f40df755b5dab447a2d5299
tdounias <tdounias@reed.edu>
2017-04-08 15:41:21 -0700

    Create string vectors for future handling

--- fbi_data_handling.R
+++ fbi_data_handling.R
@@ -1,19 +1,21 @@
-test <- readLines(file("C:\\Users\\tdounias\\Downloads\\HC 2013 (1)\\HC 2013.txt", open = "r"), skipNul = TRUE)
+full_fbi <- readLines(file("C:\\Users\\tdounias\\Downloads\\HC 2013 (1)\\HC 2013.txt", open = "r"), skipNul = TRUE)
 
-file.choose()
 
-head(test)
 
-?readLines()
-#12 spaces ignored
-
-new_vec <- vector()
+fbi_reporting <- vector()
+fbi_incidents <- vector()
 j <- 1
+z <- 1
 
-for(i in seq_along(test)){
-  if(substr(test[i], 1, 1) == "B"){
-    new_vec[j] <- test[i]
+#creates vector with only reporting precincts
+for(i in seq_along(full_fbi)){
+  if(substr(full_fbi[i], 1, 1) == "B" && substr(full_fbi[i], 218, 225) != "        "){
+    fbi_reporting[j] <- full_fbi[i]
     j <- j + 1
+  }
+  if(substr(full_fbi[i], 1, 1) == "I"){
+  fbi_incidents[z] <- full_fbi[i]
+  z <- z + 1
   }
 }
 
