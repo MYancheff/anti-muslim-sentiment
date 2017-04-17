3fc4b9b2b21b9a1ec157778e8aa11eb52c7f2f53
tdounias <tdounias@reed.edu>
2017-04-16 18:43:10 -0700

    Added second viz posibility

--- visualizations.R
+++ visualizations.R
@@ -7,6 +7,8 @@ reporting_df <- read.csv("C:\\Users\\tdounias\\Desktop\\Reed College\\Spring 201
 
 
 #Visualization 1
+
+#Year, Metro Area, Number of Incidents
 reporting_df_1 <- reporting_df %>%
   group_by(Metro_Area, Master_File_Year) %>%
   summarize(Incidents = sum(Number_of_Incidents_Year, na.rm = TRUE))
@@ -16,6 +18,14 @@ reporting_df_1$Master_File_Year <- as.factor(reporting_df_1$Master_File_Year)
 
 ggplot(reporting_df_1, aes(Master_File_Year, Incidents, col = Metro_Area, fill = Metro_Area)) +
   geom_point()
+
+#OR
+
+#Year, number of Incidents, Race of Offender
+incidents_df_1 <- incidents_df %>%
+  mutate(Year = year(Incident_Date), count = 1) %>%
+  group_by(Year, Offender_Race) %>%
+  summarize(Number_of_Incidents = sum(count))
 
 #Visualization 2
 
