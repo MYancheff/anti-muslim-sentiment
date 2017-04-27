5703aea260570457579d521157032fb4730068e0
Emily Clark <emclark@reed.edu>
2017-04-27 14:41:48 -0700

    write.csv

---
+++ StateCensus/CensusSources.R
@@ -0,0 +1,13 @@
+Our analysis pulls census data from three surveys: 
+  1)	American Community Survey (ACS) 2011-2015 (5-Year Estimates)
+a)	Population Data
+i)	Total Population: SE_T002_001
+ii)	Population Density: SE_T002_002
+b)	Median Income
+i)	Median Household Income (In 2015 Inflation Adjusted Dollars): SE_T057_00
+2)	UCR Crime Data 2010
+a)	Violent Crime
+i)	Number of Total Violent Crimes: SE_T004_001
+3)	US Religion Data (RCMS) 2010
+a)	Number of Adherents per State
+b)	Number of Congregations per State
